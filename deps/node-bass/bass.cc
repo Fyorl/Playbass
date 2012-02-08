a6c49018503e736e807a156829bb33d63c4acd6b
Fyorl <gaelreth@gmail.com>
2012-02-08 21:53:27 +0800

    Tab and music sync properly, keyboard commands added.

--- deps/node-bass/bass.cc
+++ deps/node-bass/bass.cc
@@ -4,29 +4,87 @@
 
 using namespace v8;
 
-Handle<Value> bass_play (const Arguments& args) {
+Handle<Value> bass_stream_pause (const Arguments& args) {
 	HandleScope scope;
 	
-	Local<External> field = Local<External>::Cast(args.This()->GetInternalField(0));
-	HSTREAM* stream = static_cast<HSTREAM*>(field->Value());
-	BASS_ChannelPlay(*stream, FALSE);
+	DWORD stream = args.This()->GetInternalField(0)->Uint32Value();
+	BASS_ChannelPause(stream);
 	
 	return scope.Close(Undefined());
 }
 
-Handle<Object> wrap_stream (HSTREAM* stream) {
+Handle<Value> bass_stream_play (const Arguments& args) {
+	HandleScope scope;
+	
+	DWORD stream = args.This()->GetInternalField(0)->Uint32Value();
+	BASS_ChannelPlay(stream, FALSE);
+	
+	return scope.Close(Undefined());
+}
+
+Handle<Value> bass_stream_stop (const Arguments& args) {
+	HandleScope scope;
+	
+	DWORD stream = args.This()->GetInternalField(0)->Uint32Value();
+	BASS_ChannelStop(stream);
+	
+	return scope.Close(Undefined());
+}
+
+Handle<Value> bass_stream_sync (const Arguments& args) {
+	HandleScope scope;
+	
+	double ms = args[0]->NumberValue();
+	DWORD stream = args.This()->GetInternalField(0)->Uint32Value();
+	QWORD bytes = BASS_ChannelSeconds2Bytes(stream, ms / 1000);
+	BASS_ChannelSetPosition(stream, bytes, BASS_POS_BYTE);
+	
+	return scope.Close(Undefined());
+}
+
+Handle<Value> bass_stream_time (const Arguments& args) {
+	HandleScope scope;
+	
+	DWORD stream = args.This()->GetInternalField(0)->Uint32Value();
+	QWORD bytes = BASS_ChannelGetPosition(stream, BASS_POS_BYTE);
+	double secs = BASS_ChannelBytes2Seconds(stream, bytes);
+	
+	return scope.Close(Number::New(secs));
+}
+
+Handle<Object> wrap_stream (HSTREAM stream) {
 	HandleScope scope;
 	Handle<FunctionTemplate> tpl = FunctionTemplate::New();
 	Handle<ObjectTemplate> obj_tpl = tpl->InstanceTemplate();
 	obj_tpl->SetInternalFieldCount(1);
 	
-	tpl->PrototypeTemplate()->Set(
+	Local<Object> obj = obj_tpl->NewInstance();
+	obj->SetInternalField(0, Uint32::New(stream));
+	
+	obj->Set(
 		String::NewSymbol("play")
-		, FunctionTemplate::New(bass_play)->GetFunction()
+		, FunctionTemplate::New(bass_stream_play)->GetFunction()
 	);
 	
-	Local<Object> obj = obj_tpl->NewInstance();
-	obj->SetInternalField(0, External::New(stream));
+	obj->Set(
+		String::NewSymbol("pause")
+		, FunctionTemplate::New(bass_stream_pause)->GetFunction()
+	);
+	
+	obj->Set(
+		String::NewSymbol("stop")
+		, FunctionTemplate::New(bass_stream_stop)->GetFunction()
+	);
+	
+	obj->Set(
+		String::NewSymbol("time")
+		, FunctionTemplate::New(bass_stream_time)->GetFunction()
+	);
+	
+	obj->Set(
+		String::NewSymbol("sync")
+		, FunctionTemplate::New(bass_stream_sync)->GetFunction()
+	);
 	
 	return scope.Close(obj);
 }
@@ -37,6 +95,22 @@ Handle<Value> bass_init (const Arguments& args) {
 	BASS_Init(-1, 44100, 0, 0, NULL);
 	
 	return scope.Close(Undefined());
+}
+
+Handle<Value> bass_free (const Arguments& args) {
+	HandleScope scope;
+	
+	BASS_Free();
+	
+	return scope.Close(Undefined());
+}
+
+Handle<Value> bass_error (const Arguments& args) {
+	HandleScope scope;
+	
+	int err = BASS_ErrorGetCode();
+	
+	return scope.Close(Integer::New(err));
 }
 
 Handle<Value> bass_stream_create_file (const Arguments& args) {
@@ -55,7 +129,7 @@ Handle<Value> bass_stream_create_file (const Arguments& args) {
 	String::AsciiValue fname(args[0]);
 	HSTREAM stream = BASS_StreamCreateFile(FALSE, (void *) *fname, 0, 0, 0);
 	
-	return wrap_stream(&stream);
+	return wrap_stream(stream);
 }
 
 void init (Handle<Object> target) {
@@ -67,6 +141,16 @@ void init (Handle<Object> target) {
 	target->Set(
 		String::NewSymbol("load_file")
 		, FunctionTemplate::New(bass_stream_create_file)->GetFunction()
+	);
+	
+	target->Set(
+		String::NewSymbol("free")
+		, FunctionTemplate::New(bass_free)->GetFunction()
+	);
+	
+	target->Set(
+		String::NewSymbol("error")
+		, FunctionTemplate::New(bass_error)->GetFunction()
 	);
 }
 
--- playbass.js
+++ playbass.js
@@ -10,6 +10,7 @@ var TabWriter = require('./lib/TabWriter.js');
 var util = require('util');
 
 var TICK_INTERVAL = 50;
+var FFW_INTERVAL = 1000;
 var current_tick = 0;
 
 process.on('uncaughtException', function (err) {
@@ -20,8 +21,23 @@ process.on('uncaughtException', function (err) {
 
 process.on('SIGINT', function () {
 	ncurses.cleanup();
+	BASS.free();
 	process.exit(0);
 });
+
+function ms_to_time (ms) {
+	var s = Math.floor(ms / 1000);
+	ms -= s * 1000;
+	var m = Math.floor(s / 60);
+	
+	if (m < 10) m = '0' + m;
+	if (s < 10) s = '0' + s;
+	if (ms < 10) ms = '00' + ms;
+	else if (ms < 100) ms = '0' + ms;
+	
+	ms = ms.toString();
+	return m + ':' + s + ':' + ((ms.indexOf('.') > -1) ? ms.substring(0, ms.indexOf('.')) : ms);
+}
 
 function main () {
 	var stdwin = new ncurses.Window();
@@ -29,22 +45,104 @@ function main () {
 	var stream;
 	var tab = null;
 	var interval = null;
+	var state = 0;
 	
 	BASS.init();
+	
+	var pause = function () {
+		// Decrease the tick by 1 for syncing purposes
+		state = 0;
+		stream.pause();
+		clearInterval(interval);
+	};
+	
+	var play = function () {
+		if (current_tick > 0) {
+			current_tick--;
+		}
+		
+		state = 1;
+		stream.sync(current_tick * TICK_INTERVAL);
+		stream.play();
+		main_loop();
+		interval = setInterval(main_loop, TICK_INTERVAL);
+	};
+	
+	var quit = function () {
+		ncurses.cleanup();
+		BASS.free();
+		process.exit(0);
+	};
+	
+	var jump = function (ms) {
+		pause();
+		current_tick += (ms / TICK_INTERVAL);
+		
+		if (current_tick < 0) {
+			current_tick = 0;
+		}
+		
+		stream.sync(current_tick * TICK_INTERVAL);
+		tab.reset(current_tick, TICK_INTERVAL);
+		update_times();
+	};
+	
+	var key_handler = function (ch, code) {
+		switch (code) {
+			case ncurses.keys.SPACE:
+				if (!state) {
+					play();
+				} else {
+					pause();
+				}
+			break;
+			
+			case ncurses.keys.ESC:
+				quit();
+			break;
+			
+			case ncurses.keys.LEFT:
+				jump(-FFW_INTERVAL);
+			break;
+			
+			case ncurses.keys.RIGHT:
+				jump(FFW_INTERVAL);
+			break;
+			
+			case ncurses.keys.UP:
+				jump(FFW_INTERVAL * 5);
+			break;
+			
+			case ncurses.keys.DOWN:
+				jump(FFW_INTERVAL * 5 * -1);
+			break;
+		}
+		
+		switch (ch.toLowerCase()) {
+			case 'r':
+				jump(-1 * (current_tick * TICK_INTERVAL));
+			break;
+		}
+	};
+	
+	var update_times = function () {
+		stdwin.print(6, 0, ' tick time: ' + ms_to_time(current_tick * TICK_INTERVAL));
+		stdwin.print(7, 0, ' BASS time: ' + ms_to_time(stream.time() * 1000));
+		stdwin.refresh();
+	};
 	
 	var main_loop = function () {
 		tab.write(current_tick, TICK_INTERVAL);
 		current_tick++;
+		update_times();
 	};
 	
 	selector.on('selected', function (fname) {
-		stream = BASS.load_file(fname);
+		stream = BASS.load_file('assets/songs/' + fname);
 		tab = new TabWriter.Writer(stdwin);
 		
 		tab.on('initialised', function () {
-			stream.play();
-			main_loop();
-			interval = setInterval(main_loop, TICK_INTERVAL);
+			stdwin.on('inputChar', key_handler);
 		});
 		
 		tab.initialise(
